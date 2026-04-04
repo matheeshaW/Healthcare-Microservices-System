cc579429115d162bf78ec9f6a2be417756b7f66f
kanushka <kanushkakahakotuwa2001@gmail.com>
2026-04-04 15:53:21 +0530

    add appointment routes for retrieving user and doctor appointments

--- appointment-service/src/routes/appointment.routes.js
+++ appointment-service/src/routes/appointment.routes.js
@@ -3,8 +3,12 @@ const router = express.Router();
 
 const {
   createAppointment,
+  getMyAppointments,
+  getDoctorAppointments,
 } = require("../controllers/appointment.controller");
 
 router.post("/", createAppointment);
+router.get("/my", getMyAppointments);
+router.get("/doctor", getDoctorAppointments);
 
 module.exports = router;
