c2a9d5b059438ef4afd7173e8a9b30bafe1e62b0
kanushka <kanushkakahakotuwa2001@gmail.com>
2026-04-04 17:15:43 +0530

    add update and cancel appointment routes

--- appointment-service/src/routes/appointment.routes.js
+++ appointment-service/src/routes/appointment.routes.js
@@ -5,10 +5,13 @@ const {
   createAppointment,
   getMyAppointments,
   getDoctorAppointments,
+  updateAppointmentStatus,
+  cancelAppointment,
 } = require("../controllers/appointment.controller");
 
 router.post("/", createAppointment);
 router.get("/my", getMyAppointments);
 router.get("/doctor", getDoctorAppointments);
-
+router.put("/:id/status", updateAppointmentStatus);
+router.delete("/:id", cancelAppointment);
 module.exports = router;
