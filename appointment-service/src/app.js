c0ebb9a2292344ddbb9dac421254fa7ac6f6998a
kanushka <kanushkakahakotuwa2001@gmail.com>
2026-04-13 17:12:26 +0530

    improve database connection handling and server startup logic

--- appointment-service/src/app.js
+++ appointment-service/src/app.js
@@ -14,14 +14,15 @@ app.get("/", (req, res) => {
   res.send("Appointment Service is Running");
 });
 
-// DB connection
-mongoose.connect(process.env.MONGO_URI)
-  .then(() => console.log("MongoDB connected"))
-  .catch(err => console.log(err));
-
-// Server
 const PORT = process.env.PORT || 5003;
-
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
-});
+mongoose.connect(process.env.MONGO_URI)
+  .then(() => {
+    console.log("MongoDB connected");
+    app.listen(PORT, () => {
+      console.log(`Server running on port ${PORT}`);
+    });
+  })
+  .catch(err => {
+    console.error(err);
+    process.exit(1);
+  });
