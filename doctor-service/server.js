8257ebace2ce3b82663fdd0369c388f25af33716
niRmana11 <nirmanaherath123@gmail.com>
2026-04-15 23:02:25 +0530

    Fix the doctor profile creation errors in dashboard and profile pages

--- doctor-service/server.js
+++ doctor-service/server.js
@@ -1,9 +1,7 @@
 const dotenv = require("dotenv");
+dotenv.config();
 const app = require("./src/app");
 const connectDB = require("./src/config/db");
-
-// load environment variables
-dotenv.config();
 
 const PORT = process.env.PORT || 5002;
 
