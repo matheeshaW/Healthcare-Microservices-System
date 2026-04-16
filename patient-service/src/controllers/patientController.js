3fcbe09397b61915a92a10c1e2ddfcf11710126f
kanushka <kanushkakahakotuwa2001@gmail.com>
2026-04-16 14:29:54 +0530

    add getUserById function to patientController

--- patient-service/src/controllers/patientController.js
+++ patient-service/src/controllers/patientController.js
@@ -1,4 +1,5 @@
 const PatientProfile = require("../models/PatientProfile");
+const User = require("../models/User");
 
 // CREATE or UPDATE profile
 exports.upsertProfile = async (req, res) => {
@@ -52,6 +53,27 @@ exports.getProfile = async (req, res) => {
       data: profile
     });
 
+  } catch (err) {
+    res.status(500).json({ error: err.message });
+  }
+};
+
+exports.getUserById = async (req, res) => {
+  try {
+    const { userId } = req.params;
+    const user = await User.findById(userId).select("name role");
+
+    if (!user) {
+      return res.status(404).json({
+        success: false,
+        message: "User not found"
+      });
+    }
+
+    res.json({
+      success: true,
+      data: user
+    });
   } catch (err) {
     res.status(500).json({ error: err.message });
   }
